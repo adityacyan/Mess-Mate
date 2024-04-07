e712a94e099705f71d3903633bcee4183ae86792
adityacyan <cntsgg@gmail.com>
2024-04-07 16:48:28 +0530

    Changes to be committed: 	modified:   userpage4.js

--- userpage4.js
+++ userpage4.js
@@ -1,5 +1,5 @@
 function buycupons() {
-    const amountstr = prompt("how much cupon u want to buy?")
+    const amountstr = prompt("how much coupons u want to buy?")
     const amount= Number(amountstr)
     console.log(amount)
     
